4d1ee78a05760e76cd65b97ba4d12ed725049077
Bolo Chen <bolosstuff12@gmail.com>
2022-06-23 16:22:01 +0700

    memperbaiki style, dan memberikan function agar kalkulator bekerja sebagai mestinya.

--- script.js
+++ script.js
@@ -17,7 +17,47 @@ const calculator = {
   }
    
   function inputDigit(digit) {
+    if (calculator.displayNumber === '0'){
+      calculator.displayNumber = digit;
+    } else {
     calculator.displayNumber += digit;
+    }
+  }
+
+  function inverseNumber() {
+    if (calculator.displayNumber === '0'){
+      return;
+    }
+    calculator.displayNumber *= -1;
+  }
+
+  function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+      calculator.operator = operator;
+      calculator.waitingForSecondNumber = true;
+      calculator.firstNumber = calculator.displayNumber;
+
+      // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+      calculator.displayNumber = '0';
+    } else {
+      alert('Operator sudah ditetapkan');
+    }
+  }
+
+  function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+      alert('Anda belum menetapkan operator');
+      return;
+    }
+
+    let result = 0;
+    if (calculator.operator === '+'){
+      result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+      result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    calculator.displayNumber = result;
   }
    
   const buttons = document.querySelectorAll('.button');
@@ -27,6 +67,29 @@ const calculator = {
       // mendapatkan objek elemen yang diklik
       const target = event.target;
    
+      if(target.classList.contains('clear')){
+        clearCalculator();
+        updateDisplay();
+        return;
+      }
+
+      if(target.classList.contains('negative')){
+        inverseNumber();
+        updateDisplay();
+        return;
+      }
+
+      if(target.classList.contains('equals')){
+        performCalculation();
+        updateDisplay();
+        return;
+      }
+
+      if (target.classList.contains('operator')) {
+        handleOperator(target.innerText)
+        return;
+      }
+
       inputDigit(target.innerText);
       updateDisplay();
     });
